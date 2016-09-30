b36f8e52b8294c5020a0fb4016d7155742d306e0
Peter Walter Tafirenyika Chifamba <peter.chifamba@live.com>
2016-09-30 16:10:11 +0200

    Nuget packages
    Added major packages reuired for this project.

---
+++ Win2DCAD/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using Win2DCAD.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace Win2DCAD.Views
+    {
+    public sealed partial class DetailPage :Page
+        {
+        public DetailPage()
+            {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+            }
+        }
+    }
+
